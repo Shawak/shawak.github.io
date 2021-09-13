05628c8360f95e596d624054eb8d62d07c25f7ca
Maximilian <Shawak@users.noreply.github.com>
2021-09-13 23:53:37 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
+var renderer = new marked.Renderer();
+renderer.link = function(href, title, text) {
+    var link = marked.Renderer.prototype.link.apply(this, arguments);
+    return link.replace("<a", "<a target='_blank'");
+};
+
 marked.setOptions({
-    linksInNewTab: true
+    renderer: renderer
 });
 
 for (const element of document.querySelectorAll('.markdown')) {
