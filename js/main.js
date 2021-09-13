3583873cd0ae97e0a683542cdb94bd903463cfe7
Maximilian <Shawak@users.noreply.github.com>
2021-09-13 13:00:25 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+marked.setOptions({
+    linksInNewTab: true
+});
+
 for (const element of document.querySelectorAll('.markdown')) {
     element.innerHTML = marked(element.innerHTML.trim());
     element.style.opacity = 1;
